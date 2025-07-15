b9f657423682100dbfa7849d3051a729a63a0eaf
hasanshiwantek <hasan.shiwantek@gmail.com>
2025-07-16 00:54:27 +0500

    Added Stats and OrderTable Component

--- src/app/components/home/Home.tsx
+++ src/app/components/home/Home.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import SetupProgress from "./SetupProgress";
+import Stats from "./Stats";
+import OrderTable from "./OrderTable";
 const Home = () => {
   return (
     <>
       <div className=" p-10  bg-[#f6f7f9]">
-      <h1 className="text-[#34313f] !text-3xl !font-medium my-5">Get Started Guides</h1>
-        <SetupProgress />
+        <h1 className="text-[#34313f] !text-3xl !font-medium my-5">
+          Get Started Guides
+        </h1>
+        <main className="flex flex-col gap-5">
+          <SetupProgress />
+          <Stats />
+          <OrderTable/>
+        </main>
       </div>
     </>
   );
