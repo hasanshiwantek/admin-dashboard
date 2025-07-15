801934cfbb06b057101cf70655661e84e40b8ae6
Faizan <faizanshamsi2017@gmail.com>
2025-07-15 23:38:02 +0500

    minor fix

--- src/app/components/layout/LayoutWrapper.tsx
+++ src/app/components/layout/LayoutWrapper.tsx
@@ -6,12 +6,18 @@ interface LayoutWrapperProps {
   children: React.ReactNode;
 }
 
-const LayoutWrapper: React.FC<LayoutWrapperProps> = () => {
+const LayoutWrapper: React.FC<LayoutWrapperProps> = ({ children }) => {
   return (
-    <>
-      <div>
-        <Header />
-        <SideBar/>
+     <>
+      <Header />
+      <div className="flex  h-[calc(100vh-5.5rem)] overflow-hidden">
+        <div className="w-[26.8rem] shrink-0 h-full overflow-y-auto">
+          <SideBar />
+        </div>
+
+        <main className="flex-1 overflow-y-auto bg-white">
+          {children}
+        </main>
       </div>
     </>
   );
