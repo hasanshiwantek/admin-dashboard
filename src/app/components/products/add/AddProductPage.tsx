cd837f085648574217bdd8d7242e6763613ee8f4
hasanshiwantek <hasan.shiwantek@gmail.com>
2025-07-19 02:17:19 +0500

    Made Save/Cancel button UI

--- src/app/components/products/add/AddProductPage.tsx
+++ src/app/components/products/add/AddProductPage.tsx
@@ -4,31 +4,42 @@ import { FormProvider, useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import SidebarNavigation from "./SidebarNavigation";
 import BasicInfoForm from "./BasicInformation";
-
+import Link from "next/link";
+import { FaArrowLeftLong } from "react-icons/fa6";
 export default function AddProductPage() {
-    const methods = useForm();
-    const onSubmit = methods.handleSubmit((data) => console.log(data));
+  const methods = useForm();
+  const onSubmit = methods.handleSubmit((data) => console.log(data));
 
-    return (
-        <div>
-            <div className=" items-center mb-4">
-              <h1 className="!text-5xl !font-extralight !text-gray-600 !my-5">
+  return (
+    <div>
+      <div className=" items-center mb-4">
+        <Link href={"/manage/products"} className="flex items-center justify-start gap-2 text-gray-500">
+          <FaArrowLeftLong size={18} />
+          <h2 className="!text-gray-500">VIEW PRODUCTS</h2>
+        </Link>
+        <h1 className="!text-5xl !font-extralight !text-gray-600 !my-5">
           Add Product
         </h1>
-        <hr className="my-5"/>
+        <hr className="my-5" />
+      </div>
+      <div className="flex">
+        <SidebarNavigation />
+        <FormProvider {...methods}>
+          <form
+            onSubmit={onSubmit}
+            className="flex-1 overflow-y-auto p-6 space-y-8"
+          >
+            <BasicInfoForm />
+            {/* <DescriptionEditor /> */}
+            {/* <ImagesUploader /> */}
+            {/* ...more */}
+            <div className="flex justify-end  gap-10 items-center fixed  bottom-0 right-0 bg-white/90 z-10 shadow-xs border-t w-full p-4">
+                <button className="btn-outline-primary">Cancel</button>
+            <button className="btn-primary" type="submit">Save Product</button>
             </div>
-            <div className="flex">
-                <SidebarNavigation />
-                <FormProvider {...methods}>
-                    <form onSubmit={onSubmit} className="flex-1 overflow-y-auto p-6 space-y-8">
-                        <BasicInfoForm />
-                        {/* <DescriptionEditor /> */}
-                        {/* <ImagesUploader /> */}
-                        {/* ...more */}
-                        <Button type="submit">Save Product</Button>
-                    </form>
-                </FormProvider>
-            </div>
-        </div>
-    );
+          </form>
+        </FormProvider>
+      </div>
+    </div>
+  );
 }
