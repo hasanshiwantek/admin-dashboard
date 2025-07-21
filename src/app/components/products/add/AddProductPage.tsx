1a6a736fad78b681ab38b06b1d2046ccfe176f5c
Faizan <faizanshamsi2017@gmail.com>
2025-07-22 00:20:13 +0500

    dimensions page added

--- src/app/components/products/add/AddProductPage.tsx
+++ src/app/components/products/add/AddProductPage.tsx
@@ -10,6 +10,7 @@ import ImageVideoUploader from "./ImageVideoUploader";
 import ProductIdentifiers from "./ProductIdentifiers";
 import Inventory from "./Inventory";
 import Seo from "./Seo";
+import Dimensions from "./Dimensions";
 
 export default function AddProductPage() {
   const methods = useForm();
@@ -43,6 +44,7 @@ export default function AddProductPage() {
             <ProductIdentifiers />
             {/* <Inventory /> */}
             <Seo />
+            <Dimensions />
             <div className="flex justify-end  gap-10 items-center fixed  bottom-0 right-0 bg-white/90 z-10 shadow-xs border-t w-full p-4">
               <button className="btn-outline-primary">Cancel</button>
               <button className="btn-primary" type="submit">
