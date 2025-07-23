3d2c06dd0b73965b98c3cb1cc426803811ec8b65
hasanshiwantek <hasan.shiwantek@gmail.com>
2025-07-24 00:14:54 +0500

    Added SKU Export pge in AddProduct

---
+++ src/app/(protected)/manage/products/export-skus/page.tsx
@@ -0,0 +1,14 @@
+import ExportPage from '@/app/components/products/export-skus/ExportPage'
+import React from 'react'
+const page = () => {
+  return (
+    <>
+    <div className='p-10'>
+        <ExportPage/>
+
+    </div>
+    </>
+  )
+}
+
+export default page
