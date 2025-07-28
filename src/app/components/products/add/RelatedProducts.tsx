34157513fff11b8915e4c9e167bb9b1ed7ff8e25
Faizan <faizanshamsi2017@gmail.com>
2025-07-28 19:17:26 +0500

    minor changes

--- src/app/components/products/add/RelatedProducts.tsx
+++ src/app/components/products/add/RelatedProducts.tsx
@@ -1,5 +1,5 @@
 // BasicInfoForm.tsx
-import { useFormContext } from "react-hook-form";
+import { useFormContext, Controller } from "react-hook-form";
 import { Checkbox } from "@/components/ui/checkbox"
 import { Label } from "@/components/ui/label"
 import { HiQuestionMarkCircle } from "react-icons/hi2";
@@ -13,26 +13,39 @@ import {
 
 export default function RelatedProducts() {
 
-    const { register } = useFormContext();
+    const { register, control } = useFormContext();
  
     return (
         <div id="relatedProducts" className="p-10 bg-white shadow-lg rounded-sm ">
             <h1 >Related Products</h1>
-            <div className="flex items-center space-x-2 mt-6 ">
-                <Checkbox id="relatedProducts" {...register("relatedProducts")} />
-                <Label htmlFor="relatedProducts">Automatically show related products on my storefront
-                    <TooltipProvider>
-                        <Tooltip>
-                            <TooltipTrigger asChild>
-                                <HiQuestionMarkCircle />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                When enabled, we'll find related products for you. Untick this to surface the ability select specific products.
-                            </TooltipContent>
-                        </Tooltip>
-                    </TooltipProvider>
-                </Label>
-            </div>
+            <Controller
+                control={control}
+                name="relatedProducts"
+                defaultValue={false}
+                render={({ field }) => (
+                    <div className="flex items-center space-x-2 mt-6">
+                        <Checkbox
+                            id="relatedProducts"
+                            checked={field.value}
+                            onCheckedChange={(val) => field.onChange(val === true)}
+                        />
+                        <Label htmlFor="relatedProducts">
+                            Automatically show related products on my storefront
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger asChild>
+                                        <HiQuestionMarkCircle />
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        When enabled, we'll find related products for you. Untick this to surface the ability to select specific products.
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                        </Label>
+                    </div>
+                )}
+            />
+
         </div>
     );
 }
