68f74997f9854593caa0f545a4dbb7c149e323f1
hasanshiwantek <hasan.shiwantek@gmail.com>
2025-07-29 18:59:28 +0500

    Added Import Page and export page

--- src/app/(protected)/manage/products/import/page.tsx
+++ src/app/(protected)/manage/products/import/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
+// import ImportCsv from '@/app/components/products/import/ImportCSV'
 import ImportCsv from '@/app/components/products/import/ImportCSV'
-
 const page = () => {
   return (
     <div><ImportCsv/></div>
