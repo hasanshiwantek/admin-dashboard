a600e1b7db89a2a89b9011a7e3f8c64073fe7c35
hasanshiwantek <hasan.shiwantek@gmail.com>
2025-08-05 02:02:53 +0500

    Added * for all image urls

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    images: {
-    domains: ["ecom.brokercell.com","images.pexels.com","images.unsplash.com"],
+  images: {
+    // domains: ["ecom.brokercell.com","images.pexels.com","images.unsplash.com"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
