7a844848b2681f7da51e08286a62d6a55c387bda
hasanshiwantek <hasan.shiwantek@gmail.com>
2025-09-01 19:10:19 +0500

    Integrated Edit Order API in AddOrders

--- src/app/(protected)/manage/orders/edit/[id]/page.tsx
+++ src/app/(protected)/manage/orders/edit/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
 import OrderForm from "@/app/components/orders/add/forms/OrderForm";
-export default function page({ params }: { params: { id: string } }) {
+export default function page({ params }: { params: { id: any } }) {
   return <OrderForm orderId={params.id} />;
 }
