97e929e459bb54186413abcbb026ddc1a7192e1f
hasanshiwantek <hasan.shiwantek@gmail.com>
2025-12-17 23:40:42 +0500

    Added Dradt order page

---
+++ src/app/(protected)/manage/orders/draft/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import DraftOrder from '@/app/components/orders/draft/DraftOrder'
+const page = () => {
+  return (
+    <div className='p-10'>
+        <DraftOrder/>
+    </div>
+  )
+}
+
+export default page
