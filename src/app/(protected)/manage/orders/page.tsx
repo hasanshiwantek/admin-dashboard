38b5a61c5190a717fa14d4896db2d33e2559e115
Areeb Hussain <areeb.hussain@shiwantek.com>
2026-04-09 08:35:07 -0700

    commented code

--- src/app/(protected)/manage/orders/page.tsx
+++ src/app/(protected)/manage/orders/page.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import styles from "@/styles/orders/Order.module.css";
 import AllOrders from "@/app/components/orders/AllOrders";
 const page = () => {
+  const getDeviceType = () => {
+    if (typeof window === "undefined") return "desktop";
+
+    const userAgent = navigator.userAgent;
+
+    if (/mobile/i.test(userAgent)) return "mobile";
+    if (/tablet/i.test(userAgent)) return "tablet";
+
+    return "desktop";
+  };
+
   return (
     <>
       <div>
