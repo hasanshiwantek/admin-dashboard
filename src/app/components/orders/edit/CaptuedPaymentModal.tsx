3bad2aa27ae70a30e4d63af316434f748d6086f9
Areeb Hussain <areeb.hussain@shiwantek.com>
2026-05-18 08:33:58 -0700

    view shipping modal view fixes

--- src/app/components/orders/edit/CaptuedPaymentModal.tsx
+++ src/app/components/orders/edit/CaptuedPaymentModal.tsx
@@ -40,7 +40,7 @@ export default function ConfirmationModal({
     return (
         <Dialog open={open} onOpenChange={handleClose}>
             <DialogContent
-                className="sm:max-w-[1100px] p-0 gap-0 rounded-none [&>button]:text-white [&>button]:top-3 [&>button]:right-3"
+                className="sm:max-w-[900px] p-0 gap-0 rounded-none [&>button]:text-white [&>button]:top-3 [&>button]:right-3"
 
                 onEscapeKeyDown={handleClose}
                 onPointerDownOutside={handleClose}
