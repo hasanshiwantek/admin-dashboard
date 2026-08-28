601340f07bc9325a5e6a221d632bbeb8d7d526f2
areebhussainshiwantek <areeb.hussain@shiwantek.com>
2026-08-28 05:09:09 -0700

    Clean up SettingsPage by removing website settings
    Removed commented out code for website settings.

--- src/app/components/settings/SettingsPage.tsx
+++ src/app/components/settings/SettingsPage.tsx
@@ -50,12 +50,12 @@ const SettingsPage = () => {
   const initialTab = searchParams.get("tab") || "website";
 
   const generalItems = [
-    {
-      id: "website",
-      title: "Website",
-      description:
-        "Physical dimension settings, search engine optimization, HTTPS, search engine robots",
-    },
+    // {
+    //   id: "website",
+    //   title: "Website",
+    //   description:
+    //     "Physical dimension settings, search engine optimization, HTTPS, search engine robots",
+    // },
     // {
     //   id: "display",
     //   title: "Display",
